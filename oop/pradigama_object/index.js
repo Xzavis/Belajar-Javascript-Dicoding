d80b73cb4bfd3c58790bba1d7105ffd6f8c3a66a
xza <xzaabdulmalikibrahim354@gmail.com>
2024-11-24 23:01:42 +0700

    belajar OOP pradigma part 1 jam 23:00

---
+++ oop/pradigama_object/index.js
@@ -0,0 +1,45 @@
+// Paradigma Berbasis Objek
+// Sesuai dengan namanya, di paradigma ini kita akan banyak berhubungan dengan objek. 
+// Object-oriented programming (OOP) adalah paradigma pemrograman yang memiliki pendekatan berbasis object. 
+// Object akan berinteraksi satu sama lain untuk menyelesaikan tugas sehingga membentuk keseluruhan program. 
+// Selain itu, object merupakan representasi dari entitas.
+
+
+// Object terdiri dari atribut informasi (property) dan perilaku (method). 
+// Property adalah informasi tentang objek tersebut seperti nama, warna, dan jenis, 
+// sedangkan method adalah aksi atau perilaku yang dapat dilakukan oleh objek seperti 
+// berjalan, berlari, dan terbang. Kita ambil contoh pada kehidupan sehari-hari, 
+// misalnya entitas kucing direpresentasikan menjadi object kucing dengan memiliki properti dan atribut. 
+// Property pada kucing adalah warna, jenis ras, nama, dan umur. 
+// Method pada kucing adalah berlari, tidur, makan, dan mencakar. 
+
+// Misalkan, Anda memiliki dua ekor kucing dengan nama Ziggy dan Garfield. Untuk setiap kucing, 
+// kita perlu untuk menentukan nama, warna, berat badan, dan perilakunya. 
+// 
+// Kucing pertama, kita mendefinisikan namanya adalah Ziggy, 
+// warnanya hitam putih, berat badannya 3 kg, 
+// dan perilakunya adalah suka tidur. Kemudian, 
+// kita melakukan hal yang sama untuk kucing kedua, Garfield. 
+// Kita mendefinisikan namanya Garfield, warnanya oranye, berat badannya 2 kg, 
+// dan perilakunya adalah suka bermain. Setiap memiliki kucing baru, 
+// kita akan melakukan hal ini secara berulang.
+
+// Object adalah bentuk nyata dari suatu entitas, sedangkan class adalah cetak biru (blueprint), 
+// cetakan atau template yang dapat kita gunakan berulang kali untuk membuat object. 
+// Object dan class mempermudah ketika ingin membuat entitas yang kompleks dengan cepat dan efektif. 
+// Anda perlu untuk mencermati kedua hal ini agar bisa menguasai paradigma OOP.
+
+// https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/academy/dos-6c41a555cb4fdf81dd1b83047001c8f620240730151902.jpeg
+
+// +-------+ CLASS +--------+                   +-----------+ Object +-----------+ 
+// |  Property |    Method  |                   |    Property    |     Method    |
+// |------------------------|                   |----------------|---------------|
+// |    Name   |  Bermain() |        ===>       | Name= Garfield |   Bermain()   |
+// |   Color   |   Makan()  |                   | Color= orage   |     Eat()     |
+// |   Type    |   Tidur()  |                   |  Type= Persia  |    Sleep()    |
+// +------------------------+                   +--------------------------------+
+
+
+// Karena object dan class memiliki peran sangat penting di paradigma OOP, 
+// kita akan membahas cara membuatnya dengan dua cara di bawah ini.
+
